d41b2b59e6dac3ba140191ac6484c9b6be3d95a5
Adam Harvey <aharvey@php.net>
2016-05-26 11:12:47 -0500

    Switch to a more easily debugged setup.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,5 +13,51 @@
 
 Route::get('/', function () {
     $tweeps = DB::table('tweeps')->paginate(30);
-    return view('index', ['tweeps' => $tweeps]);
+    ob_start();
+
+?>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>#phptek tweeps</title>
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap-theme.min.css">
+  </head>
+  <body role="document">
+    <div class="container">
+      <h1>#phptek tweeps</h1>
+
+      <?= $tweeps->links() ?>
+
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Account</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($tweeps as $tweep): ?>
+            <tr>
+              <td><?= $tweep->account ?></td>
+              <td><?= $tweep->name ?></td>
+            </tr>
+          <?php endforeach ?>
+        </tbody>
+      </table>
+
+      <?= $tweeps->links() ?>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
+<?php
+
+    return ob_get_clean();
 });
